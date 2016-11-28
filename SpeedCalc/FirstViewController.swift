223b1045e96305bd2f7dce9e5dc50ad96e6aea1d
Valar <Valar@npcompete.net>
2016-11-28 09:51:01 +0530

    Initial commit

---
+++ SpeedCalc/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SpeedCalc
+//
+//  Created by valar on 28/11/16.
+//  Copyright © 2016 valar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
